cc34dbb079b6eb37f568912747de8672f5014fcd
kangirfan14 <irfanmaulana140999@gmail.com>
2026-07-15 18:02:07 +0700

    Update agenda-data.js

--- agenda-data.js
+++ agenda-data.js
@@ -19,18 +19,29 @@ const DATABASE_AGENDA = [
     location: "Yayasan Pendidikan Arifin Billah, Weru, Cirebon",
     desc: "Pada Acara HAFLAH AKHIRUSSANAH ARIFIN BILLAH 2026.",
     banner: "IMG_8437.JPG", // Ganti dengan nama file foto banner Anda
-    docLink: "#"
+    docLink: "https://karangsariinfo.cirebonid.workers.dev/agenda"
   },
   
  {
-    id: "2",
+    id: "3",
     title: "NYABAR VOL.3 TAHUN 2026",
     start: "2026-07-10 18:00",
     end: "2026-07-11 00:00",
     location: "Area lapang Gor Karangsari",
     desc: "Acara Pemutaran Film, Pameran Mini, Video Mapping, Performance, Doorprize.",
     banner: "poster nybr.png", // Ganti dengan nama file foto banner Anda
-    docLink: "#"
+    docLink: "https://karangsariinfo.cirebonid.workers.dev/agenda"
+  },
+
+   {
+    id: "4",
+    title: "JELAWE BERSATU - LOMBA MINIATUR ARAK - ARAKAN 2026",
+    start: "2026-08-17 07:00",
+    end: "2026-08-17 17:05",
+    location: "Jelawe Lor, Karangsari, Weru, Kab. Cirebon",
+    desc: "Memperingati HUT - RI ke 81, warga blok jelawe Lor kompak adakan acara.",
+    banner: "JELAWE 20262.png", // Ganti dengan nama file foto banner Anda
+    docLink: "https://karangsariinfo.cirebonid.workers.dev/agenda"
   },
   
 ];
